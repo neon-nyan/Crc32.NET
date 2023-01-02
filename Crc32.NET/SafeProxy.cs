82b3d4e61c933a9ce69ca36d9a2fcde612025747
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-01-03 05:45:47 +0700

    Remove unused codes

--- Crc32.NET/SafeProxy.cs
+++ Crc32.NET/SafeProxy.cs
@@ -12,112 +12,72 @@ using System;
 
 namespace Force.Crc32
 {
-	internal class SafeProxy
-	{
-		private const uint Poly = 0xedb88320u;
+    internal class SafeProxy
+    {
+        private const uint Poly = 0xedb88320u;
 
-		private readonly uint[] _table = new uint[16 * 256];
+        private readonly uint[] _table = new uint[16 * 256];
 
-		internal SafeProxy()
-		{
-			Init(Poly);
-		}
+        internal SafeProxy()
+        {
+            Init(Poly);
+        }
 
-		protected void Init(uint poly)
-		{
-			var table = _table;
-			for (uint i = 0; i < 256; i++)
-			{
-				uint res = i;
-				for (int t = 0; t < 16; t++)
-				{
-					for (int k = 0; k < 8; k++) res = (res & 1) == 1 ? poly ^ (res >> 1) : (res >> 1);
-					table[(t * 256) + i] = res;
-				}
-			}
-		}
+        protected void Init(uint poly)
+        {
+            var table = _table;
+            for (uint i = 0; i < 256; i++)
+            {
+                uint res = i;
+                for (int t = 0; t < 16; t++)
+                {
+                    for (int k = 0; k < 8; k++) res = (res & 1) == 1 ? poly ^ (res >> 1) : (res >> 1);
+                    table[(t * 256) + i] = res;
+                }
+            }
+        }
 
-#if !NETCORE20 && !NETCORE30
-		public uint Append(uint crc, byte[] input, int offset, int length)
-		{
-			uint crcLocal = uint.MaxValue ^ crc;
-
-			uint[] table = _table;
-			while (length >= 16)
-			{
-				var a = table[(3 * 256) + input[offset + 12]]
-					^ table[(2 * 256) + input[offset + 13]]
-					^ table[(1 * 256) + input[offset + 14]]
-					^ table[(0 * 256) + input[offset + 15]];
-
-				var b = table[(7 * 256) + input[offset + 8]]
-					^ table[(6 * 256) + input[offset + 9]]
-					^ table[(5 * 256) + input[offset + 10]]
-					^ table[(4 * 256) + input[offset + 11]];
-
-				var c = table[(11 * 256) + input[offset + 4]]
-					^ table[(10 * 256) + input[offset + 5]]
-					^ table[(9 * 256) + input[offset + 6]]
-					^ table[(8 * 256) + input[offset + 7]];
-
-				var d = table[(15 * 256) + ((byte)crcLocal ^ input[offset])]
-					^ table[(14 * 256) + ((byte)(crcLocal >> 8) ^ input[offset + 1])]
-					^ table[(13 * 256) + ((byte)(crcLocal >> 16) ^ input[offset + 2])]
-					^ table[(12 * 256) + ((crcLocal >> 24) ^ input[offset + 3])];
-
-				crcLocal = d ^ c ^ b ^ a;
-				offset += 16;
-				length -= 16;
-			}
-
-			while (--length >= 0)
-				crcLocal = table[(byte)(crcLocal ^ input[offset++])] ^ crcLocal >> 8;
-
-			return crcLocal ^ uint.MaxValue;
-		}
-#else
         public uint Append(uint crc, byte[] input, int offset, int length)
         {
             return Append(crc, input.AsSpan(offset, length));
         }
 
         public uint Append(uint crc, ReadOnlySpan<byte> input)
-		{
-			uint crcLocal = uint.MaxValue ^ crc;
+        {
+            uint crcLocal = uint.MaxValue ^ crc;
 
-			uint[] table = _table;
-			while (input.Length >= 16)
-			{
-				var a = table[(3 * 256) + input[12]]
-					^ table[(2 * 256) + input[13]]
-					^ table[(1 * 256) + input[14]]
-					^ table[(0 * 256) + input[15]];
+            uint[] table = _table;
+            while (input.Length >= 16)
+            {
+                var a = table[(3 * 256) + input[12]]
+                    ^ table[(2 * 256) + input[13]]
+                    ^ table[(1 * 256) + input[14]]
+                    ^ table[(0 * 256) + input[15]];
 
-				var b = table[(7 * 256) + input[8]]
-					^ table[(6 * 256) + input[9]]
-					^ table[(5 * 256) + input[10]]
-					^ table[(4 * 256) + input[11]];
+                var b = table[(7 * 256) + input[8]]
+                    ^ table[(6 * 256) + input[9]]
+                    ^ table[(5 * 256) + input[10]]
+                    ^ table[(4 * 256) + input[11]];
 
-				var c = table[(11 * 256) + input[4]]
-					^ table[(10 * 256) + input[5]]
-					^ table[(9 * 256) + input[6]]
-					^ table[(8 * 256) + input[7]];
+                var c = table[(11 * 256) + input[4]]
+                    ^ table[(10 * 256) + input[5]]
+                    ^ table[(9 * 256) + input[6]]
+                    ^ table[(8 * 256) + input[7]];
 
-				var d = table[(15 * 256) + ((byte)crcLocal ^ input[0])]
-					^ table[(14 * 256) + ((byte)(crcLocal >> 8) ^ input[1])]
-					^ table[(13 * 256) + ((byte)(crcLocal >> 16) ^ input[2])]
-					^ table[(12 * 256) + ((crcLocal >> 24) ^ input[3])];
+                var d = table[(15 * 256) + ((byte)crcLocal ^ input[0])]
+                    ^ table[(14 * 256) + ((byte)(crcLocal >> 8) ^ input[1])]
+                    ^ table[(13 * 256) + ((byte)(crcLocal >> 16) ^ input[2])]
+                    ^ table[(12 * 256) + ((crcLocal >> 24) ^ input[3])];
 
-				crcLocal = d ^ c ^ b ^ a;
+                crcLocal = d ^ c ^ b ^ a;
                 input = input.Slice(16);
             }
 
             var i = 0;
-			while (i < input.Length)
-				crcLocal = table[(byte)(crcLocal ^ input[i++])] ^ crcLocal >> 8;
+            while (i < input.Length)
+                crcLocal = table[(byte)(crcLocal ^ input[i++])] ^ crcLocal >> 8;
 
-			return crcLocal ^ uint.MaxValue;
-		}
-#endif
-	}
+            return crcLocal ^ uint.MaxValue;
+        }
+    }
 }
